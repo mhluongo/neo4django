94fab3d05591424f2769bd2b143f464dc6823e2d
Matt Luongo <mhluongo@gmail.com>
2014-01-11 19:48:44 -0500

    Don't use neo4j-rest-client over 1.9, since it...
    breaks Python 2.6 compatibility. We'll save that for the next minor
    version.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     install_requires=[
         'decorator>=3.3.1',
         'python-dateutil>=2.0',
-        'neo4jrestclient>=1.7',
+        'neo4jrestclient>=1.7, <2.0',
         'Django>=1.3, <1.6',
         'lucene-querybuilder>=0.1.6'
     ],
